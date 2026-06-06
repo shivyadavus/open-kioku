cc2d33e8186f890349c0f04e5dbfb5897cbae669
Shiv Yadav <shivyadav.us@gmail.com>
2026-06-05 19:05:03 -0500

    Bump workspace packages and plugins to version 2.0.1

--- Formula/open-kioku.rb
+++ Formula/open-kioku.rb
@@ -1,25 +1,25 @@
 class OpenKioku < Formula
   desc "Local-first code intelligence MCP for AI coding agents"
   homepage "https://github.com/shivyadavus/open-kioku"
-  version "2.0.0"
+  version "2.0.1"
   license "Elastic-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/shivyadavus/open-kioku/releases/download/v2.0.0/ok-macos-arm64"
+      url "https://github.com/shivyadavus/open-kioku/releases/download/v2.0.1/ok-macos-arm64"
       sha256 "2db7ab0b77293988349b27da72cbdde2f37ddf5d897ef13ec3020cadc3fb90e0"
     else
-      url "https://github.com/shivyadavus/open-kioku/releases/download/v2.0.0/ok-macos-x86_64"
+      url "https://github.com/shivyadavus/open-kioku/releases/download/v2.0.1/ok-macos-x86_64"
       sha256 "5bbcd44ccfee2fd304fc1887939e6374830c365802684cf46af79e9453ccc4a7"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/shivyadavus/open-kioku/releases/download/v2.0.0/ok-linux-arm64"
+      url "https://github.com/shivyadavus/open-kioku/releases/download/v2.0.1/ok-linux-arm64"
       sha256 "1a88a5031ef0c66a704c2c7f5469c0ed5f1a808da2baea305083e3237b9619b3"
     else
-      url "https://github.com/shivyadavus/open-kioku/releases/download/v2.0.0/ok-linux-x86_64"
+      url "https://github.com/shivyadavus/open-kioku/releases/download/v2.0.1/ok-linux-x86_64"
       sha256 "ecc4c75971333587cb8fb662783f3623fd659ce03f29c153692ec231b2c62cd4"
     end
   end
